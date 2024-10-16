e26a00d0198be78e33ee94f13bb12192b745a0cd
Sadra <sadrarezaei4@gmail.com>
2024-10-16 14:01:36 +0330

    first commit

---
+++ Circle City.py
@@ -0,0 +1,19 @@
+n , m = input().split()
+flag = 2
+n = [int(n) for n in input().split()]
+m = [int(m) for m in input().split()]
+for i in n:
+    if i == 0:
+        if flag == 1:
+            print("YES")
+            break
+        else:
+            flag = 0
+    elif i == 1:
+        if flag == 0:
+            print("YES")
+            break
+        else:
+            flag = 1
+if flag == 2:
+    print("NO")
